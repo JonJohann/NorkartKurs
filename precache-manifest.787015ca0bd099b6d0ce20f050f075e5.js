f3ef162afc886ccfc1955f6d9b324794629bf2dd
Jonathan Johannessen <jonathsj@stud.ntnu.no>
2020-11-02 17:55:02 +0100

    Updates

---
+++ precache-manifest.787015ca0bd099b6d0ce20f050f075e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6312faf118b0adf3e3bc8662fbeed038",
+    "url": "/NorkartKurs/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/NorkartKurs/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "b19875e811582a0a8334",
+    "url": "/NorkartKurs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/NorkartKurs/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NorkartKurs/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19875e811582a0a8334",
+    "url": "/NorkartKurs/static/js/main.4ed77537.chunk.js"
+  },
+  {
+    "revision": "61cd76ff60203a5b8c8a",
+    "url": "/NorkartKurs/static/js/runtime-main.debddde7.js"
+  }
+]);
